e22a5eb17e402bc8d6faa3c185c8a4f67101de7c
Roystone-Were <patorankingquavo100@gmail.com>
2024-09-17 16:30:41 +0300

    First Commit

---
+++ intro.py
@@ -0,0 +1,10 @@
+name = input("What is your Name?\n") # --\n-- prints to Next line
+
+#removes whitespaces on strings
+name = name.strip() 
+
+#capitalizes String
+name = name.capitalize()
+
+print(f"Hello, {name}") 
+
